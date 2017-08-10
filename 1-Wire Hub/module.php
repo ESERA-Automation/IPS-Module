80bf765171042cec802634d7bc3222ffb2a94d7a
ESERA-Automation <support@esera-automation.de>
2017-08-10 17:15:14 +0200

    update 1-Wire Hub

--- 1-Wire Hub/module.php
+++ 1-Wire Hub/module.php
@@ -7,8 +7,8 @@ class Esera1WireHub extends IPSModule {
 
         //These lines are parsed on Symcon Startup or Instance creation
         //You cannot use variables here. Just static values.
-        $this->CreateVariableProfile("ESERA.SpannungV", 2, " V", 0, 15, 0, 2, "");
-        $this->CreateVariableProfile("ESERA.StrommA", 2, " mA", 0, 100, 0, 2, "");
+        $this->CreateVariableProfile("ESERA.Spannung15V", 2, " V", 0, 15, 0.1, 2, "");
+        $this->CreateVariableProfile("ESERA.Strom1500mA", 2, " mA", 0, 1500, 0, 0, "");
 
         $this->RegisterPropertyInteger("OWDID", 1);
 
