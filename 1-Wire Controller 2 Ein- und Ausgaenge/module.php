3ed13b0dad81cdb41968171dc988b5d9df6f3d3e
ESERA-Automation <support@esera-automation.de>
2017-08-10 18:32:48 +0200

    update

--- 1-Wire Controller 2 Ein- und Ausgaenge/module.php
+++ 1-Wire Controller 2 Ein- und Ausgaenge/module.php
@@ -11,7 +11,6 @@ class EseraOneWireController2SYS extends IPSModule {
         $this->CreateVariableProfile("ESERA.Spannung10V", 2, " V", 0, 10, 0.1, 2, "");
 
         $this->RegisterVariableFloat("AnalogOut", "Analog Out", "ESERA.Spannung10V");
-        $this->RegisterVariableInteger("SYS", "SYS", "");
 
         for($i = 1; $i <= 4; $i++){
           $this->RegisterVariableBoolean("Input".$i, "Input".$i, "~Switch");
@@ -22,7 +21,7 @@ class EseraOneWireController2SYS extends IPSModule {
     			$this->EnableAction("Output".$i);
     		}
 
-        $this->ConnectParent("{FCABCDA7-3A57-657D-95FD-9324738A77B9}"); //1Wire Controller
+        //$this->ConnectParent("{FCABCDA7-3A57-657D-95FD-9324738A77B9}"); //1Wire Controller
     }
     public function Destroy(){
         //Never delete this line!
