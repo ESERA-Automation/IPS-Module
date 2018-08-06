0d9e66cac89e10a1656458375a651da2543bb0ce
ESERA-Automation <support@esera-automation.de>
2018-08-06 19:50:26 +0200

    update group

--- OWD Group Command/module.php
+++ OWD Group Command/module.php
@@ -55,7 +55,7 @@ class OWDGroupCommand extends IPSModule {
 
 	//Gruppenbefehle
 	public function SetGroupOut(int $Number, string $function, int $Value) {
-		$this->Send("SET,OWD,GRP,". $Number .",". $Function ..",". $Value ."");
+		$this->Send("SET,OWD,GRP,". $Number .",". $Function .",". $Value ."");
 		//$this->Send("SET,OWD,GRP,". $GroupNumber .","."SHT".",". $Value ."");
 		$this->SendDebug("OWDGroupCommand", "GruppenNumber:" . $data->$Number . " | Function: " . $data->$function .  "| Value: " . $data->Value);
 	
