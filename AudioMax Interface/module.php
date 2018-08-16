3ef4ea71d3dddc51102eb036fbf4ccf062974974
ESERA-Automation <support@esera-automation.de>
2018-08-16 08:26:19 +0200

    update audiomax

--- AudioMax Interface/module.php
+++ AudioMax Interface/module.php
@@ -48,7 +48,9 @@ class AudioMaxServer extends IPSModule {
 	public function ForwardData($JSONString){
 
 		$data = json_decode($JSONString);
+
 		$this->SendDebug("FWD", $data->Command, 0);
+
 		$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $data->Command .chr(13))));
 
 	}
@@ -64,12 +66,12 @@ class AudioMaxServer extends IPSModule {
 			} else if ($KeepAliveInterval > 240) {
 				$KeepAliveInterval = 240;
 			}
-			$this->Send("SET,KAL,1");										//Befehlsstring des AudioMax Server
+			$this->Send("SET,SYS,KALSEND,1");
 			//$this->Send("SET,SYS,KALSENDTIME,$KeepAliveInterval");
 		} else {
-			$this->Send("SET,KAL,0");										//Befehlsstring des AudioMax Server
+			$this->Send("SET,SYS,KALSEND,0");
 		}
-/*
+
 		//ReceiveKeepAlive ein-/ausschalten
 		if ($this->ReadPropertyBoolean("ReceiveKeepAlive")) {
 			$KeepAliveInterval = $this->ReadPropertyInteger("ReceiveKeepAliveInterval");
@@ -80,19 +82,27 @@ class AudioMaxServer extends IPSModule {
 				$KeepAliveInterval = 240;
 			}
 			$this->SetTimerInterval("KeepAliveHeartbeatTimer", $KeepAliveInterval * 1000);
-			$this->Send("SET;SYS;KALREC;1");
-			$this->Send("SET;SYS;KALRECTIME;$KeepAliveInterval");
+			$this->Send("SET,SYS,KALREC,1");
+			//$this->Send("SET,SYS,KALRECTIME,$KeepAliveInterval");
 		} else {
-			$this->Send("SET;SYS;KALREC;0");
+			$this->Send("SET,SYS,KALREC,0");
 			$this->SetTimerInterval("KeepAliveHeartbeatTimer", 0);
 		}
+
+		//Datenausgabe konfigurieren
+		/*switch($this->ReadPropertyString("DataOutputType")) {
+			case 'OWD':
+				$this->Send("SET,OWB,OWDID,0");
+				break;
+
+			case 'ID':
+				$this->Send("SET,OWB,OWDID,1");
+				break;
+		}
+
+		$this->SaveToSRAM();
 */
-		
-		//$this->SaveToSRAM();
 	}
-	
-	
-	
 	public function ReceiveData($JSONString) {
 
 		$data = json_decode($JSONString);
@@ -121,89 +131,136 @@ class AudioMaxServer extends IPSModule {
 
 	}
 	
-	//Daten von Schnittstelle auswerten und auf interne IPS Variablen schreiben
 	private function AnalyseData($DataString) {
+		$dataArray = explode(",", $DataString);
 
-		$dataArray = explode("|", $DataString);
+		$head = $dataArray[0]; 			//Name der übergebenen Variable
+		   $this->SendDebug("head", $head, 0);
+		
+		//SYS Variablen
+		$type = $dataArray[1]; 		//Daten der übergebenen Variable		   
+		$data = $dataArray[2]; 		//Daten der übergebenen Variable	
+		
+		//AUDIO Variablen
+		$RoomNumber = $dataArray[1]; 		//Daten der übergebenen Variable
+		$dataType = $dataArray[2]; 		//Daten der übergebenen Variable
+		$value = $dataArray[3]; 		//Daten der übergebenen Variable
 
-		$head = $dataArray[0]; //Name der übergebenen Variable
-		$value = $dataArray[1]; //Daten der übergebenen Variable
-		$type = SubStr($head, 2, 3);
-
-		switch ($type) {
-			case "KAL":
-			case "DEBUG":
-			case "ECHO":
-			case "PUSHBUTTON":
-			case "AUTOSTART":
-				$type = SubStr($head, 2, 5);
-				break;
-
-			case "FW":
-			case "HW":
-				$type = SubStr($head, 2, 2);
-				break;
-
-			case "AUDIO":
-				$headArray = explode("_", $head);
-				$deviceNumber = intval(substr($headArray[1], 3));
-				if (sizeof($headArray) >= 3){
-					$dataPoint = intval($headArray[2]);
+				   
+		/*
+		$type = SubStr($head, 1, 3);			//vorher 2,3
+           $this->SendDebug("type", $type, 0);
+		*/   
+		   
+		switch ($head) {
+			case "EVT":	
+		       $this->SendDebug("type", $type, 0);			   	
+			   $this->SendDebug("data", $data, 0);
+			   
+				switch ($type) {
+					case "KAL":		
+					break;			
 				}
-				else{
-					$dataPoint = 0;
-				}
+				$variableType = $this->GetVariableType($type);
+				$variablenID = 0;
 
-				$this->SendDebug("SendToDevice", json_encode(Array("DataID" => "{4DF6D73D-8592-40DD-87FD-54D14F36692A}", "DeviceType" => "AUDIO", "DataSource:" . $data->DataSource . " | DataRoom:" . $data->DataRoom . " | DataType:" . $data->DataType . " | Value: " . $data->Value)), 0);
-				$this->SendDataToChildren(json_encode(Array("DataID" => "{4DF6D73D-8592-40DD-87FD-54D14F36692A}", "DeviceType" => "AUDIO", "DataSource:" . $data->DataSource . " | DataRoom:" . $data->DataRoom . " | DataType:" . $data->DataType . " | Value: " . $data->Value)));
+				//Erstellen der Variablen
+				switch ($variableType){
+					case 1:
+					    if ($type == "KAL"){
+						$variablenID = $this->RegisterVariableboolean($head,"Heartbeat AudioMax-Server","",100);
+						}
+						break;
+					case 2:
+						//$variablenID = $this->RegisterVariableboolean($type, $type);
+						if ($type == "PWR"){
+							$variablenID = $this->RegisterVariableboolean($type,"AudioMax Power","~Switch",1);
+						}
+						break;
+					case 3:
+						$variablenID = $this->RegisterVariableString($type, $type);
+						break;
+
+					default:
+						$this->SendDebug("RegisterVariable", "Unbekannter Variablentyp", 0);
+				}
+				//Wert setzen
+				if ($variablenID !== 0) {
+					SetValue($variablenID, $data);
+				}							
+			    return;	
+
 				
+			case "SYS": 	   
+		       $this->SendDebug("type", $type, 0);			   	
+			   $this->SendDebug("data", $data, 0);
+			   
+				switch ($type) {
+					case "KAL":
+					case "PWR":
+					case "DEBUG":
+					case "ECHO":
+					case "PUSHBUTTON":
+					case "AUTOSTART":			
+					break;
+					
+					case "SW":
+					case "HW":
+					case "FW":
+					break;
+				}
+				
+				$variableType = $this->GetVariableType($type);
+				$variablenID = 0;
+
+				//Erstellen der Variablen
+				switch ($variableType){
+					case 1:
+						$variablenID = $this->RegisterVariableInteger($type, $type);
+						break;
+					case 2:
+						//$variablenID = $this->RegisterVariableboolean($type, $type);
+						if ($type == "PWR"){
+							$variablenID = $this->RegisterVariableboolean($type,"AudioMax Power","~Switch",1);
+						}
+						break;
+					case 3:
+						$variablenID = $this->RegisterVariableString($type, $type);
+						break;
+
+					default:
+						$this->SendDebug("RegisterVariable", "Unbekannter Variablentyp", 0);
+				}			
+
+				//Wert setzen
+				if ($variablenID !== 0) {
+					SetValue($variablenID, $data);
+				}							
+			    return;	
+				
+			
+			case "AUDIO":						
+		           $this->SendDebug("roomnumber", $RoomNumber, 0);
+		           $this->SendDebug("datatype", $dataType, 0);
+		           $this->SendDebug("value", $value, 0);				
+				
+				//geändert 10.08.2017 andrge (hinweis von ch. schrader)		
+				$this->SendDebug("SendToDevice", json_encode(Array("DataID" => "{6B6E9D9E-4541-48CD-9F01-EFE52ACB2530}", "DeviceType" => "AUDIO", "RoomNumber" => $RoomNumber, "DataType" => $dataType, "Value" => $value)), 0);
+				$this->SendDataToChildren(json_encode(Array("DataID" => "{6B6E9D9E-4541-48CD-9F01-EFE52ACB2530}", "DeviceType" => "AUDIO", "RoomNumber" => $RoomNumber, "DataType" => $dataType, "Value" => $value)));
 				return;
 
-			default:
-			
-				if(SubStr($head, 18, 1) == '_' || StrLen($head) == 18) {
-					$type = "OWDID";
-				}
-			
-		}
-
-		$variableType = $this->GetVariableType($type);
-		$variablenID = 0;
-
-		//Erstellen der Variablen
-		switch ($variableType){
-			case 1:
-				$variablenID = $this->RegisterVariableInteger($head, $head);
-				break;
-
-			case 3:
-				$variablenID = $this->RegisterVariableString($head, $head);
-				break;
-
-			default:
-				$this->SendDebug("RegisterVariable", "Unbekannter Variablentyp", 0);
-		}
-
-		//Wert setzen
-		if ($variablenID !== 0) {
-			SetValue($variablenID, $value);
-		}
-
-		//Wenn Time dann automatisch Zeit korrigieren
-		if ($type == "TIME") {
-			$this->FixTime($value);
 		}
 
 	}
 	
-	//ab hier Kommunikation zwischen IPS und AudioMax Server
-	//------------------------------------------------------
 	
-	//System Herzschlag senden
+	//KAL Senden
 	public function SendKeepAliveHeartbeat() {
-		$this->Send("".$this->ReadPropertyInteger("AudioMaxID")."_KAL|1");
+		$this->Send("SET,KAL|1");
 	}
 	
+	//GET Funktionen
+	//--------------------------------------------	
 	//System Debug Mode abfragen
 	public function GetSysAMDebug() {
 		$this->Send("GET,SYS,DEBUG");
@@ -214,7 +271,7 @@ class AudioMaxServer extends IPSModule {
 	}
 	//System Pushbuttom Mode abfragen
 	public function GetSysAMPushbutton() {
-		$this->Send("SET,SYS,PUSHBUTTON");
+		$this->Send("GET,SYS,PUSHBUTTON");
 	}
 	//System Autostart Mode abfragen
 	public function GetSysAMAutostart() {
@@ -224,29 +281,65 @@ class AudioMaxServer extends IPSModule {
 	public function GetSysAMPwr() {
 		$this->Send("GET,SYS,PWR");
 	}
-	//System KAL Mode abfragen
-	public function GetSysAMKal() {
-		$this->Send("GET,KAL");
-	}	
-	
+	//System Firmware abfragen
+	public function GetSysAMFW() {
+		$this->Send("GET,SYS,FW");
+	}
+	//System Hardware abfragen
+	public function GetSysAMHW() {
+		$this->Send("GET,SYS,HW");
+	}
+		
+	//SET Funktionen
+	//--------------------------------------------
+	//System Debug Mode setzen
+	public function SetSysAMDebug() {
+		$this->Send("SET,SYS,DEBUG");
+	}
+	//System Echo Mode setzen
+	public function SetSysAMEcho() {
+		$this->Send("SET,SYS,ECHO");
+	}
+	//System Pushbuttom Mode setzen
+	public function SetSysAMPushbutton() {
+		$this->Send("SET,SYS,PUSHBUTTON");
+	}
+	//System Autostart Mode setzen
+	public function SetSysAMAutostart() {
+		$this->Send("SET,SYS,AUTOSTART");
+	}
+	//System Power Status setzen
+	public function SetSysAMPwr() {
+		$this->Send("SET,SYS,PWR");
+	}
+	//System KAL Mode setzen
+	public function SetSysAMKal() {
+		$this->Send("SET,SYS,KAL");
 
+	}
+
+	
 	//Liefert den Typ der Variable abhängig von der empfangenen Daten
 	private function GetVariableType($Type) {
 
 		switch($Type){
 			//Integer
-			case "KAL":
+			case "KAL":			
 			case "DEBUG":
 			case "ECHO":
 			case "PUSHBUTTON":
 			case "AUTOSTART":
-			case "PWR":
+			case "HW":
+			case "SW":
+			case "FW":
 				return 1;
 
+			//Boolean
+			case "PWR":
+			    return 2;
+				
 			//String
-			case "ARTIKELNUMMER":
-			case "HW":
-			case "FW":
+			case "SERNO":
 				return 3;
 
 			//Unbestimmt
@@ -255,7 +348,6 @@ class AudioMaxServer extends IPSModule {
 				return false;
 		}
 	}
-	
 	public function GetConfigurationForParent() {
 
 		//Vordefiniertes Setup der seriellen Schnittstelle
