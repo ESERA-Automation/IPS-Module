64d6a14e80f0886937adfd6abe37bca718c0fc69
ESERA-Automation <support@esera-automation.de>
2018-08-16 09:01:07 +0200

    update audiomax

--- AudioMax Multiroom/module.php
+++ AudioMax Multiroom/module.php
@@ -8,6 +8,7 @@ class AudioMaxServer extends IPSModule {
  		//These lines are parsed on Symcon Startup or Instance creation
 		//You cannot use variables here. Just static values.
 		
+		/*
 		$this->CreateVariableProfile("ESERA.AM_Volume",1," dB",40,0,1,2,"%");
 		$this->CreateVariableProfile("ESERA.AM_Gain",1," dB",0,15,1,2,"%");
 	    $this->CreateVariableProfile("ESERA.AM_Tone",1," dB",0,15,1,2,"%");
@@ -42,7 +43,7 @@ class AudioMaxServer extends IPSModule {
 
 		$this->RegisterVariableboolean($type,"AudioMax Power","~Switch",1);
         $this->EnableAction("pwr".$i);
-		
+		*/
 
 		$this->RegisterPropertyInteger("ConnectionType", 10);
 		$this->RegisterPropertyString("DataOutputType", "AUDIO");
